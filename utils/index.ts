c5d4fafc5c99a62f8ca96feadb467d513cd01950
Nikhil <nikhilpraveen7777@gmail.com>
2024-02-19 23:18:25 +0530

    Added Car cards

---
+++ utils/index.ts
@@ -0,0 +1,28 @@
+export async function fetchCars(){
+    const headers ={
+        'X-RapidAPI-Key': 
+        '5c5357a4c8msh08eed39856fed0cp1d052cjsnc0734c66f669',
+		'X-RapidAPI-Host':
+         'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{
+        headers: headers,
+    });
+    const result = await response.json()
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; 
+    const mileageFactor = 0.1; 
+    const ageFactor = 0.05;
+  
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
+  
